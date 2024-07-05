99bd94a72d0e8a4a02725dd9303ee0a2b1d900bf
JaroDomcio <281053@student.pwr.edu.pl>
2024-07-05 18:38:36 +0200

    Added cannonical url

--- blog/models.py
+++ blog/models.py
@@ -17,8 +17,6 @@ class Post(models.Model): #model postu
         PUBLISHED = 'PB', 'Opublikowany'
 
     title = models.CharField(max_length=250) #Pole posta
-    # slug = models.SlugField(max_length=250,
-    #                         unique_for_date='publish')
     slug = models.SlugField(max_length=250,
                             unique_for_date='publish')
     author = models.ForeignKey(User,
@@ -42,8 +40,10 @@ class Post(models.Model): #model postu
 
     def __str__(self):
         return self.title
-    # def get_absolute_url(self):
-    #     return reverse('blog:post_detail',args=[self.publish.year,
-    #                                             self.publish.month,
-    #                                             self.publish.day,
-    #                                             self.slug])
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 from .models import Post
 
 
@@ -9,10 +8,14 @@ def post_list(request):
                   'blog/post/list.html',
                   {'posts':posts})
 
-def post_detail(request,id):
+def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,
-                            id=id,
-                            status=Post.Status.PUBLISHED)
+                            status=Post.Status.PUBLISHED,
+                            slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day
+                            )
     return render(request,
                   'blog/post/detail.html',
                   {'post':post})
