4cbf17f914a5dd4b863d1014894f9c4eff90e103
unknown <dominikdab3@gmail.com>
2024-08-10 22:09:30 +0200

    Comment system implemented

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 app_name='blog'
 urlpatterns = [
-    #widoki posta
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
+    path('<int:post_id>/comment/',
+         views.post_comment, name='post_comment')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.views.decorators.http import require_POST
+
+from .models import Post,Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
+from .forms import CommentForm
 
 def post_list(request):
     post_list= Post.published.all()
@@ -16,6 +20,12 @@ def post_list(request):
                   'blog/post/list.html',
                   {'posts':posts})
 
+# class post_list(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,
                             status=Post.Status.PUBLISHED,
@@ -24,6 +34,20 @@ def post_detail(request,year,month,day,post):
                             publish__month=month,
                             publish__day=day
                             )
+    comments = post.comments.filter(active=True)
+    form=CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post':post})
+                  {'post':post, 'comments':comments, 'form':form})
+
+
+@require_POST
+def post_comment(request,post_id):
+    post = get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
+    comment= None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request,'blog/post/comment.html', {'post':post,'comment':comment,'form':form})
