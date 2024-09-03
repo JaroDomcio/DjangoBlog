d56948d8c67422e5b98b1eef3d1c9daa9f5cfdb3
JaroDomcio <281053@student.pwr.edu.pl>
2024-09-03 11:28:58 +0200

    Implemented recommendations using tags

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView, DetailView
 from .forms import CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request,tag_slug=None):
@@ -42,9 +43,12 @@ def post_detail(request,year,month,day,post):
                             )
     comments = post.comments.filter(active=True)
     form=CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
-                  {'post':post, 'comments':comments, 'form':form})
+                  {'post':post, 'comments':comments, 'form':form,"similar_posts":similar_posts})
 
 
 @require_POST
