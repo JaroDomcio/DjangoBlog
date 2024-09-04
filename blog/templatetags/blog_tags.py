a7889d1715e994a0c9d0037576173f4521aa2d9b
JaroDomcio <281053@student.pwr.edu.pl>
2024-09-04 12:27:12 +0200

    Added additional statistics on the main page

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from ..models import Post
+from django.db.models import Count
+
+register = template.Library()
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=0):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+@register.inclusion_tag('blog/post/most_commented_posts.html')
+def show_most_commented_posts(count=0):
+    most_commented_posts = Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+    return {'most_commented_posts': most_commented_posts}
